14478e1e24b7a85a86c592bd3898c22127cbee04
thus1@t-online.de <thus1@t-online.de@fdeb5d1b-4c89-0112-d9c9-01d7278ad25a>
2010-06-29 22:59:14 +0000

    correct rev num

--- trunk/src/stamp-th9x.h
+++ trunk/src/stamp-th9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "30.06.2010"
-#define TIME_STR "00:39:42"
-#define SUB_VERS 1344-thus
-#define SVN_VERS "trunk-r112"
+#define TIME_STR "00:58:18"
+#define SUB_VERS 1346-thus
+#define SVN_VERS "trunk-r116"
