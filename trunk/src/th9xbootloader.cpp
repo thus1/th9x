4a724e326f828d78a8a0cfec3e66fc92313c7e51
josef.glatthaar <josef.glatthaar@fdeb5d1b-4c89-0112-d9c9-01d7278ad25a>
2010-11-15 20:20:22 +0000

    Last version

--- trunk/src/th9xbootloader.cpp
+++ trunk/src/th9xbootloader.cpp
@@ -53,33 +53,119 @@
    receiveData <<= 1;
    if(!(PORTE & (1 << INP_E_PPM_IN)))
      receiveData |= 1;
-   ++receiveBit;
    if(receiveBit & 0x8)
    {
      TIMSK &= ~(1<<OCIE1A);
+     EIFR  &= ~(1 << INTF7);
+     EIMSK |= 1 << INT7;                  // Interrupt wieder ein für die Flanke
      receiveBit = 0;
      uartBuf[uartWr++] = receiveData;
      uartWr %= sizeof(uartBuf);
    }
+   else
+     ++receiveBit;
  }
 
- ISR(TIMER0_COMP_vect, ISR_NOBLOCK) //10ms timer
- {
-   cli();
-   TIMSK &= ~(1<<OCIE0); //stop reentrance
-   sei();
-   OCR0 = OCR0 + 156;
-   lcd_clear();
-   refreshDiplay();
-   cli();
-   TIMSK |= (1<<OCIE0);
-   sei();
- }
+// ISR(TIMER0_COMP_vect, ISR_NOBLOCK) //10ms timer
+// {
+//   cli();
+//   TIMSK &= ~(1<<OCIE0); //stop reentrance
+//   sei();
+//   OCR0 = OCR0 + 156;
+//   lcd_clear();
+//   refreshDiplay();
+//   cli();
+//   TIMSK |= (1<<OCIE0);
+//   sei();
+// }
 
  ISR(INT7_vect)
  {
    TCNT1 = 16000000/19200*2/3;    //Auf 2/3 stellen
-   TIMSK |= (1<<OCIE1A);
+   if(!(PORTE & (1 << INP_E_PPM_IN)))           // Startbit
+   {
+     TIFR &=  ~(1<<OCF1A);
+     TIMSK |= (1<<OCIE1A);
+     EIMSK &= ~(1 << INT7);
+   }
+ }
+
+ bool checkEnd(void)
+ {
+   if(!(PINB & (1 << INP_B_KEY_EXT)))
+     return(true);
+   return(false);
+ }
+
+ void menu(void)
+ {
+   lcd_putsAtt(30,7,PSTR("Bootloader"),0);
+   lcd_putsAtt(20,8,PSTR("Write program to FLASH"),0);
+   lcd_putsAtt(20,9,PSTR("Write data to EEPROM"),0);
+   lcd_putsAtt(20,10,PSTR("Read program from flash"),0);
+   lcd_putsAtt(20,11,PSTR("Read data from EEPROM"),0);
+   refreshDiplay();
+ }
+
+ void writeFlash(void)
+ {
+   uint16_t uartRd = 0;
+   uint8_t page = 0;
+   bool timeout = false;
+
+   do
+   {
+     lcd_outdezAtt(40,4*8,page,0);
+
+     refreshDiplay();
+
+     uint8_t *buf = (uint8_t *)page_z_buf;
+     do
+       *buf++ = 0xff;
+     while(buf < (uint8_t *)page_z_buf + SPM_PAGESIZE);
+     buf = (uint8_t *)page_z_buf;
+     do
+     {
+       while(uartRd == uartWr)      //uartWR wird im Interrupt geschrieben
+       {
+         if(checkEnd())
+         {
+           timeout = true;
+           goto prog;
+         }
+       }
+       *buf++ = uartBuf[uartRd++];
+       uartRd %= sizeof(uartBuf);
+       // bei Timeout break;
+     }
+     while(buf < (uint8_t *)page_z_buf + SPM_PAGESIZE);
+prog:
+     boot_spm_busy_wait();
+     boot_page_erase(page);      // mindestens eine Page gekommen, dann ersten Block loeschen
+     for (uint8_t i=0; i<SPM_PAGESIZE/2; ++i)
+         boot_page_fill (i*2, page_z_buf[i]);
+     boot_spm_busy_wait();
+     boot_page_write (page++);         // Store buffer in flash page.
+   }
+   while((page < 224) && !timeout);                     //224);
+
+   boot_rww_enable ();
+   lcd_putsAtt(4,6,PSTR("fertig!"),INVERS);
+ }
+
+ void writeEEprom(void)
+ {
+
+ }
+
+ void readFlash(void)
+ {
+
+ }
+
+ void readEEprom(void)
+ {
+
  }
 
  int main(void)
@@ -92,7 +178,11 @@
    DDRF = 0x00;  PORTF = 0xff; //anain
    DDRG = 0x10;  PORTG = 0xff; //pullups + SIM_CTL=1 = phonejack = ppm_in
 
-   if(PINB & (1 << INP_B_KEY_MEN))
+   while(!(PINB & (1 << INP_B_KEY_DWN)));
+
+   lcd_init();
+
+   if((PINB & (1 << INP_B_KEY_UP)))
      __asm__ __volatile__("jmp 0x0000");
 
    /* move interrupts to bootloader section */
@@ -102,7 +192,6 @@
    MCUCR = (1<<IVSEL);
 
 
-   lcd_init();
 
 // Achtung: Bei Einstieg über normale Software sind Register nicht auf default
 
@@ -115,58 +204,22 @@
 
    TCCR1B = (1 << WGM12) | (1<<CS10);
    OCR1A = 16000000/19200;
-   TIFR &=  ~(1<<OCF1A);
-   TIMSK |= (1<<OCIE1A);
+//   TIFR &=  ~(1<<OCF1A);
+//   TIMSK |= (1<<OCIE1A);
 
 
-// Int7 ein (Any logical change on INT7 generates an interrupt request)
-   EICRB = 1 << ISC70;
+// Int7 ein (The falling edge between two samples of INTn generates an interrupt request.)
+   EICRB = 1 << ISC71;
    EIFR  &= ~(1 << INTF7);
    EIMSK |= 1 << INT7;
 
-   uint16_t uartRd = 0;
-   uint8_t page = 0;
 
    lcd_clear();
 
-   lcd_putsAtt(20,8,PSTR("Bootloader"),0);
-//   lcd_putsAtt(64-5*FW,0*FH,PSTR("ALERT"),DBLSIZE);
+   menu();
 
-   refreshDiplay();
+   writeFlash();
 
-   do
-   {
-     lcd_outdezAtt(40,4*8,page,0);
-//     lcd_outdezAtt(40,5*8,(PINB & (1 << INP_B_KEY_MEN)) ? 1:2,0);
-
-     refreshDiplay();
-//     PORTE |= 1<<OUT_E_BUZZER;
-
-     uint8_t *buf = (uint8_t *)page_z_buf;
-     do
-       *buf++ = 0xff;
-     while(buf < (uint8_t *)page_z_buf + SPM_PAGESIZE);
-     buf = (uint8_t *)page_z_buf;
-     do
-     {
-       while(uartRd == uartWr)
-         ;
-       *buf++ = uartBuf[uartRd++];
-       uartRd %= sizeof(uartBuf);
-       // bei Timeout break;
-     }
-     while(buf < (uint8_t *)page_z_buf + SPM_PAGESIZE);
-     boot_spm_busy_wait();
-     boot_page_erase(page);      // mindestens eine Page gekommen, dann ersten Block loeschen
-     for (uint8_t i=0; i<SPM_PAGESIZE/2; ++i)
-         boot_page_fill (i*2, page_z_buf[i]);
-     boot_spm_busy_wait();
-     boot_page_write (page++);         // Store buffer in flash page.
-   }
-   while(page < 3);                     //224);
-
-   boot_rww_enable ();
-   lcd_putsAtt(4,6,PSTR("fertig!"),INVERS);
    while(1);
    return 0;
  }
