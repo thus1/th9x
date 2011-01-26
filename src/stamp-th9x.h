2ac819982668e274c229a9983938621951ad0696
thus1 <thus1@t-online.de>
2011-01-26 23:51:43 +0000

    current binaries

--- src/stamp-th9x.h
+++ src/stamp-th9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "24.01.2011"
-#define TIME_STR "00:15:50"
-#define SUB_VERS 2037-thus
-#define SVN_VERS "trunk-r183"
+#define DATE_STR "27.01.2011"
+#define TIME_STR "00:15:51"
+#define SUB_VERS 2038-thus
+#define SVN_VERS "trunk-r184"
