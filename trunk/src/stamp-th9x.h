3c0e9a2f67c30d7cd1f61a6256e2e4d89cd5a945
thus1@t-online.de <thus1@t-online.de@fdeb5d1b-4c89-0112-d9c9-01d7278ad25a>
2011-08-17 22:50:43 +0000

    6-ch dsm lsb first

--- trunk/src/stamp-th9x.h
+++ trunk/src/stamp-th9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "16.08.2011"
-#define TIME_STR "09:34:25"
-#define SUB_VERS 2337-thus
+#define DATE_STR "18.08.2011"
+#define TIME_STR "00:47:35"
+#define SUB_VERS 2339-thus
 #define SVN_VERS "trunk-r204"
