eb2f22a6ec6b6d2357153305f9ce69567520e825
thus <thus@t2.(none)>
2015-10-09 00:27:28 +0200

    some new symbol-chars 0x80-0x85

--- util/xbm2lbm.rb
+++ util/xbm2lbm.rb
@@ -13,35 +13,35 @@ def main(xbm)
   end
 
   File.read(xbm)=~/_width (\d+).*_height (\d+).*\{(.*)\}/m
-  w,h,bits=$1.to_i,$2.to_i,$3
-  bits=eval "["+bits+"]"
+  w,h,xbm_bits=$1.to_i,$2.to_i,$3
+  xbm_bits=eval "["+xbm_bits+"]"
   #pp w,h,bits
 
-  wb=(w+7)/8
-  hb=(h+7)/8
+  wb=(w+7)/8 #num bytes per row    in xbm
+  hb=(h+7)/8 #num bytes per column in lbm
 
   out=Array.new(w*hb,0)
   #pp out
 
+  #xbm-format Bytes horizontal
+  # 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 .. (wb-1).7
   #
-  #   87654321 87654321 87654321 87654321     .. w
-  # 1
-  # 2
-  # 3     <----
-  # 4
-  # 5
-  # 6
-  # 7
-  # 8
+  # ---------
+  # ---------
   #
-  # .
-  # .
-  # h
-  #
+  #lbm-format Bytes vertikal 
+  # 
+  # | | | | | | | |
+  # | | | | | | | |
+  # | | | | | | | |
 
-  h.times{|yp| yb=yp/8; msk= 1 << (yp%8)
-    w.times{|xp| xb=xp/8; msk2=1 << (xp%8)
-      out[yb*w+xp]|=msk if bits[wb*yp+xb]&msk2 != 0
+  h.times{|yp| 
+    yb=yp/8;           #vert byte idx lbm
+    msk= 1 << (yp%8)   #lbm byte msk, bit0 = oben
+    w.times{|xp| 
+      xb=xp/8;         #hor byte idx xpm
+      msk2=1 << (xp%8) #xpm byte msk, bit0 = links
+      out[yb*w+xp]|=msk if xbm_bits[wb*yp+xb]&msk2 != 0
     }
   }
   #pp out
@@ -49,19 +49,31 @@ def main(xbm)
 #prog_uchar img_4sticks_18x8[] = {
 ##include "sticks_lcd.h"
 #};
+  while out[-5..-1] == [0,0,0,0,0]
+    out[-5..-1] = []
+  end
 
   File.open("#{base}.lbm","wb"){|f|
-    col=0
-    f.printf("prog_uchar APM %s[] = {\n",base.sub(/.*\//,""))
-    f.printf("%d,%d,%d,\n",w/cols,h/rows,w/cols*h/rows/8)
-    rows.times{|row|
-      (hb/rows).times{|yb| yb
-        w.times{|xp|
-          f.printf("0x%02x,",out[row*w + yb*w + xp])
-        }
-        f.puts
+    #col=0
+    f.printf("uchar_p APM %s[] = {\n",base.sub(/.*\//,""))
+    f.printf("%d,%d,%d,\n",0,0,0)
+    chars=out.length/5
+    chars.times{|ci|
+      5.times{|i|
+        f.printf("0x%02x,",out[i+ci*5])
       }
+      f.printf("// %02x\n",ci+0x20)
     }
+
+#     f.printf("%d,%d,%d,\n",w/cols,h/rows,w/cols*h/rows/8)
+#     rows.times{|row|
+#       (hb/rows).times{|yb| yb
+#         w.times{|xp|
+#           f.printf("0x%02x,",out[row*w + yb*w + xp])
+#         }
+#         f.puts
+#       }
+#     }
     f.printf("};\n")
   }
 end
